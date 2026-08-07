d0f9592645c0bf21d817ea3e07d2b6461c822446
Ozioma Love Ihezukwu <64181422+Love39@users.noreply.github.com>
2026-08-07 14:54:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,10 +49,3 @@ function showPopup(bool) {
   }
 }
 
-
-var divElement = document.getElementById('viz1786107976581');  
-              var vizElement = divElement.getElementsByTagName('object')[0];  
-              vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px'; 
-              var scriptElement = document.createElement('script');  
-              scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-              vizElement.parentNode.insertBefore(scriptElement, vizElement);  
